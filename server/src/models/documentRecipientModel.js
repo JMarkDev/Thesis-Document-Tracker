059918528c0fc0d6208d34eb6f6dd2a29e7f0cae
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-08-18 17:14:15 +0800

    document recipient model added

---
+++ server/src/models/documentRecipientModel.js
@@ -0,0 +1,55 @@
+const sequelize = require("../configs/database");
+const { DataTypes, Sequelize } = require("sequelize");
+const Document = require("./documentModel");
+
+const DocumentRecipient = sequelize.define(
+  "document_recipient",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    document_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "documents", // Ensure this matches the model name in the Document model
+        key: "id",
+      },
+      onDelete: "CASCADE",
+    },
+    office_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    received_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "document_recipient",
+  }
+);
+
+// DocumentRecipient.belongsTo(Document, {
+//   foreignKey: "document_id",
+//   onDelete: "CASCADE",
+// });
+
+module.exports = DocumentRecipient;
