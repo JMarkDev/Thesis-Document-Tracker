d7c72f79978fca632d14f90d8365e11def374c77
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-09-29 14:24:11 +0800

    fix add and update workflow controller

--- server/src/controllers/documentWorkflowController.js
+++ server/src/controllers/documentWorkflowController.js
@@ -6,6 +6,28 @@ const addWorkflow = async (req, res) => {
   const { document_type, route } = req.body;
 
   try {
+    const existWorkflow = await documentRouteModel.findOne({
+      where: {
+        document_type: document_type,
+      },
+    });
+    if (!document_type.trim()) {
+      return res
+        .status(400)
+        .json({ message: "Document Type is required", status: "error" });
+    }
+    if (existWorkflow) {
+      return res
+        .status(400)
+        .json({ message: "Document Type already exist", status: "error" });
+    }
+
+    if (route.length === 0) {
+      return res
+        .status(400)
+        .json({ message: "Please select a route", status: "error" });
+    }
+
     const workflow = await documentRouteModel.create({
       document_type: document_type,
       route: route,
@@ -57,27 +79,52 @@ const getRouteById = async (req, res) => {
   }
 };
 
-const updateRoute = (req, res) => {
+const updateRoute = async (req, res) => {
   const { id } = req.params;
   const { document_type, route } = req.body;
 
-  documentRouteModel
-    .update(
+  try {
+    // const existWorkflow = await documentRouteModel.findOne({
+    //   where: {
+    //     document_type: document_type,
+    //   },
+    // });
+
+    // if (!document_type.trim()) {
+    //   return res
+    //     .status(400)
+    //     .json({ message: "Document Type is required", status: "error" });
+    // }
+    // if (existWorkflow) {
+    //   return res
+    //     .status(400)
+    //     .json({ message: "Document Type already exist", status: "error" });
+    // }
+
+    // if (route.length === 0) {
+    //   return res
+    //     .status(400)
+    //     .json({ message: "Please select a route", status: "error" });
+    // }
+
+    const updateWorkflow = await documentRouteModel.update(
       {
         document_type: document_type,
         route: JSON.stringify(route),
         updatedAt: createdAt,
       },
       { where: { id: id } }
-    )
-    .then((route) => {
-      return res
-        .status(200)
-        .json({ route, status: "success", message: "Updated successfully" });
-    })
-    .catch((error) => {
-      return res.status(500).json({ message: error.message });
+    );
+
+    return res.status(200).json({
+      updateWorkflow,
+      status: "success",
+      message: "Updated successfully",
     });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 const searchWorkflow = async (req, res) => {
