d2e7143c279fb1e7a9f0a3d50daaaf809ed25728
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-11-13 03:32:01 +0800

    fix auto logout hooks

--- client/src/hooks/UseAutoLogout.jsx
+++ client/src/hooks/UseAutoLogout.jsx
@@ -1,17 +1,101 @@
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef, useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { logoutUser } from "../services/authSlice";
+// import api from '../api/axios'
+
+// export const UseAutoLogout = () => {
+//   const dispatch = useDispatch();
+//   const inactivityTimer = useRef(null);
+//   const [autoLogoutThreshold, setAutoLogoutThreshold] = useState(0);
+//   const INACTIVITY_LIMIT = 15 * 60 * 1000;
+
+//   useEffect(() => {
+//     const getDelay = async () => {
+//       try {
+//         const response = await api.get("/document/get-delay/1");
+//         setAutoLogoutThreshold(response.data.auto_logout_minutes);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     getDelay();
+//   }, []);
+
+//   const resetTimer = () => {
+//     clearTimeout(inactivityTimer.current);
+//     inactivityTimer.current = setTimeout(() => {
+//       dispatch(logoutUser());
+//     }, INACTIVITY_LIMIT);
+//   };
+
+//   const handleActivity = () => {
+//     resetTimer();
+//   };
+
+//   useEffect(() => {
+//     resetTimer();
+//     const events = [
+//       "mousemove",
+//       "mousedown",
+//       "keypress",
+//       "keydown",
+//       "scroll",
+//       "touchstart",
+//       "click",
+//     ];
+//     events.forEach((event) => window.addEventListener(event, handleActivity));
+
+//     return () => {
+//       events.forEach((event) =>
+//         window.removeEventListener(event, handleActivity)
+//       );
+//       clearTimeout(inactivityTimer.current);
+//     };
+//   }, []);
+
+//   return null;
+// };
+
+// // export default useAutoLogout;
+import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { logoutUser } from "../services/authSlice";
+import api from "../api/axios";
 
 export const UseAutoLogout = () => {
   const dispatch = useDispatch();
   const inactivityTimer = useRef(null);
-  const INACTIVITY_LIMIT = 5 * 60 * 1000;
+  const [autoLogoutThreshold, setAutoLogoutThreshold] = useState(0);
+
+  // Fetch the auto-logout delay once
+  useEffect(() => {
+    const getDelay = async () => {
+      try {
+        const response = await api.get("/document/get-delay/1");
+        setAutoLogoutThreshold(response.data.auto_logout_minutes);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getDelay();
+  }, []);
+
+  // Reset the inactivity timer when autoLogoutThreshold changes
+  useEffect(() => {
+    if (autoLogoutThreshold > 0) {
+      resetTimer();
+    }
+  }, [autoLogoutThreshold]);
 
   const resetTimer = () => {
     clearTimeout(inactivityTimer.current);
-    inactivityTimer.current = setTimeout(() => {
-      dispatch(logoutUser());
-    }, INACTIVITY_LIMIT);
+
+    if (autoLogoutThreshold > 0) {
+      const INACTIVITY_LIMIT = autoLogoutThreshold * 60 * 1000;
+      inactivityTimer.current = setTimeout(() => {
+        dispatch(logoutUser());
+      }, INACTIVITY_LIMIT);
+    }
   };
 
   const handleActivity = () => {
@@ -19,7 +103,6 @@ export const UseAutoLogout = () => {
   };
 
   useEffect(() => {
-    resetTimer();
     const events = [
       "mousemove",
       "mousedown",
@@ -41,5 +124,3 @@ export const UseAutoLogout = () => {
 
   return null;
 };
-
-// export default useAutoLogout;
