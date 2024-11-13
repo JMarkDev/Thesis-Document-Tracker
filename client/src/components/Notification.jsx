9484d194178d0e183f0f4d813e1f746c89a47717
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2024-11-14 03:30:31 +0800

    notification added when new registered faculty

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -1,10 +1,26 @@
 import PropTypes from "prop-types";
 import { useFormat } from "../hooks/useFormatDate";
 import { useNavigate } from "react-router-dom";
+import { getUserData } from "../services/authSlice";
+import { useSelector } from "react-redux";
+import rolesList from "../constants/rolesList";
 
 const Notification = ({ notifications, handleNotificationClick }) => {
   const { dateFormat } = useFormat();
   const navigate = useNavigate();
+  const user = useSelector(getUserData);
+
+  const handleNavigate = () => {
+    if (user?.role === rolesList.admin) {
+      navigate(`/users/faculty`);
+    } else if (user?.role === rolesList.admin_staff) {
+      navigate(`/users/faculty`);
+    } else if (user?.role === rolesList.registrar) {
+      navigate(`/esu-campus/faculties`);
+    } else if (user?.role === rolesList.campus_admin) {
+      navigate(`/esu-campus/faculties`);
+    }
+  };
 
   return (
     <div className="relative z-100 w-[320px]  shadow-lg bg-white rounded-lg ">
@@ -17,11 +33,13 @@ const Notification = ({ notifications, handleNotificationClick }) => {
             <li className="p-4 text-sm text-gray-700">No notifications</li>
           )}
           {notifications?.map(
-            ({ content, createdAt, is_read, document_id, id }) => (
+            ({ content, createdAt, is_read, document_id, id, faculty_id }) => (
               <li
                 key={id}
                 onClick={() => {
                   handleNotificationClick(id);
+                  handleNavigate(faculty_id);
+
                   {
                     document_id && navigate(`/document-details/${document_id}`);
                   }
