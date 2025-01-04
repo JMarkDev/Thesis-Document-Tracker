d88480e15225227895042cd007165018375e32e9
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2025-01-04 23:41:50 +0800

    update admin settings

--- client/src/pages/Admin/Settings/Settings.jsx
+++ client/src/pages/Admin/Settings/Settings.jsx
@@ -12,6 +12,14 @@ import {
   addEsuCampus, // Import add campus action
   updateEsuCampus, // Import update campus action
 } from "../../../services/campusSlice";
+
+import {
+  addProgram,
+  fetchPrograms,
+  deleteProgram,
+  updateProgram,
+} from "../../../services/programSlice"; // Import program actions
+
 import { toastUtils } from "../../../hooks/useToast";
 import { MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
@@ -25,6 +33,10 @@ const Settings = () => {
     (state) => state.campus
   );
 
+  const { programs: programList, status: programStatus } = useSelector(
+    (state) => state.program
+  );
+
   const [newDesignation, setNewDesignation] = useState("");
   const [editDesignation, setEditDesignation] = useState({
     id: null,
@@ -34,9 +46,20 @@ const Settings = () => {
   const [newCampus, setNewCampus] = useState(""); // State for new campus
   const [editCampus, setEditCampus] = useState({ id: null, name: "" }); // State for editing campus
 
+  const [newProgram, setNewProgram] = useState({
+    name: "",
+    abbreviation: "",
+  });
+  const [editProgram, setEditProgram] = useState({
+    id: null,
+    name: "",
+    abbreviation: "",
+  });
+
   useEffect(() => {
     dispatch(fetchDesignation());
     dispatch(fetchEsuCampuses()); // Fetch campuses on mount
+    dispatch(fetchPrograms());
   }, [dispatch]);
 
   const handleAddDesignation = () => {
@@ -82,6 +105,30 @@ const Settings = () => {
 
   const handleDeleteCampus = (id) => {
     dispatch(deleteEsuCampus({ id: id, toast: toastUtils() }));
+  };
+
+  // Handling Program CRUD operations
+  const handleAddProgram = () => {
+    console.log(newProgram);
+    if (newProgram.name.trim() && newProgram.abbreviation.trim()) {
+      dispatch(addProgram({ newProgram, toast: toastUtils() }));
+      setNewProgram({ name: "", abbreviation: "" });
+    }
+  };
+
+  const handleEditProgram = (program) => {
+    setEditProgram(program);
+  };
+
+  const handleUpdateProgram = () => {
+    if (editProgram.name.trim() && editProgram.abbreviation.trim()) {
+      dispatch(updateProgram({ editProgram, toast: toastUtils() }));
+      setEditProgram({ id: null, name: "", abbreviation: "" });
+    }
+  };
+
+  const handleDeleteProgram = (id) => {
+    dispatch(deleteProgram({ id: id, toast: toastUtils() }));
   };
 
   return (
@@ -262,6 +309,143 @@ const Settings = () => {
             </ul>
           )}
         </div>
+      </div>
+      {/* Program Container */}
+      <div className="bg-white shadow-md rounded-lg p-4">
+        <h2 className="text-xl font-semibold mb-4">WMSU-ESU Programs</h2>
+        {/* Add New Program */}
+        <div className="flex items-center gap-4 mb-6 p-4 bg-white shadow-md rounded-lg">
+          <div className="w-1/4">
+            <label
+              htmlFor="abbreviation"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Abbreviation
+            </label>
+            <input
+              type="text"
+              id="abbreviation"
+              value={newProgram.abbreviation}
+              onChange={(e) =>
+                setNewProgram({ ...newProgram, abbreviation: e.target.value })
+              }
+              placeholder="BSCS"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+            />
+          </div>
+
+          <div className="w-full">
+            <label
+              htmlFor="programName"
+              className="block text-sm font-medium text-gray-700 mb-2"
+            >
+              Program Name
+            </label>
+            <input
+              type="text"
+              id="programName"
+              value={newProgram.name}
+              onChange={(e) =>
+                setNewProgram({ ...newProgram, name: e.target.value })
+              }
+              placeholder="Bachelor of Science in Computer Science"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+            />
+          </div>
+
+          <button
+            type="button"
+            onClick={handleAddProgram}
+            className="mt-5 px-6 py-2 bg-blue-500 text-white font-medium rounded-lg shadow-md hover:bg-blue-600 focus:ring-2 focus:ring-blue-400 focus:outline-none transition"
+          >
+            Add
+          </button>
+        </div>
+
+        {/* Program List */}
+        {programStatus === "loading" ? (
+          <p className="text-gray-500">Loading programs...</p>
+        ) : (
+          <ul className="space-y-4">
+            {programList?.map((program) => (
+              <li
+                key={program.id}
+                className="flex items-center justify-between bg-gray-100 px-4 py-2 rounded-lg"
+              >
+                {editProgram.id === program.id ? (
+                  <div className="flex items-center gap-4 w-full">
+                    <input
+                      type="text"
+                      value={editProgram.abbreviation}
+                      onChange={(e) =>
+                        setEditProgram({
+                          ...editProgram,
+                          abbreviation: e.target.value,
+                        })
+                      }
+                      className="w-1/4 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                      type="text"
+                      value={editProgram.name}
+                      onChange={(e) =>
+                        setEditProgram({
+                          ...editProgram,
+                          name: e.target.value,
+                        })
+                      }
+                      className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+
+                    <button
+                      type="button"
+                      onClick={handleUpdateProgram}
+                      className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
+                    >
+                      Save
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() =>
+                        setEditProgram({
+                          id: null,
+                          name: "",
+                          abbreviation: "",
+                        })
+                      }
+                      className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                ) : (
+                  <>
+                    {/* <span>{program.name}</span> */}
+                    <span>
+                      {program.abbreviation} ({program.name})
+                    </span>
+                    <div className="flex items-center gap-2">
+                      <button
+                        type="button"
+                        onClick={() => handleEditProgram(program)}
+                        className="p-2 text-lg bg-yellow-500 bg-blue-500 hover:bg-blue-600 text-white rounded-lg hover:bg-yellow-600"
+                      >
+                        <FaEdit />
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => handleDeleteProgram(program.id)}
+                        className="p-2 text-lg bg-red-500 text-white rounded-lg hover:bg-red-600"
+                      >
+                        <MdDelete />
+                      </button>
+                    </div>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
